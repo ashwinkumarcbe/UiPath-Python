568adec7f0d26930f0a611a370b9908a2c277743
christianblandford <christianblandford@me.com>
2019-03-28 18:14:00 -0700

    Increased version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uipath",
-    version="0.0.1",
+    version="0.0.2",
     author="Christian Blandford",
     author_email="christianblandford@me.com",
     description="A python API for editing UiPath .xaml files programatically.",
